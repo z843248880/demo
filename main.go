38a688bdd682c71c10b2e60a132a0add5071e31a
z843248880 <843248880@qq.com>
2021-09-26 16:26:07 +0800

    Update main.go

--- main.go
+++ main.go
@@ -2,24 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/gin-gonic/gin"
-	"net/http"
 	"time"
 )
 
 func main() {
-	go func() {
 		for {
 			time.Sleep(time.Second)
 			fmt.Println(time.Now())
 		}
-	}()
-	r := gin.Default()
-	v1 := r.Group("/")
-	v1.GET("ecard", func(c *gin.Context) {
-		time.Sleep(time.Millisecond * 1500)
-		c.String(http.StatusOK, "csb1")
-	})
-
-	r.Run(":11133")
 }
